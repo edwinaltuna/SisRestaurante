d7bfa754e80fa1d54c7d95f0dcf07ea66cc47d50
edwinaltuna <epaulfarfanaltuna@hotmail.com>
2016-10-16 17:22:42 -0500

    cambio en la pagina maestra

---
+++ SistemaRestaurante/C1_Presentacion/Controllers/IntranetController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace C1_Presentacion.Controllers
+{
+    public class IntranetController : Controller
+    {
+        //
+        // GET: /Intranet/
+
+        public ActionResult Principal()
+        {
+            return View();
+        }
+
+    }
+}
