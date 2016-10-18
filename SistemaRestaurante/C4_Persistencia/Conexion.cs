111b826f8e6e11da5e6c4a39a9f2795fae158bae
Kenneth Ubaldo Lujan Rosas <elgenio.03@gmail.com>
2016-10-18 18:19:42 -0500

    te la creiste we :v

--- SistemaRestaurante/C4_Persistencia/Conexion.cs
+++ SistemaRestaurante/C4_Persistencia/Conexion.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +9,22 @@ namespace C4_Persistencia
 {
     public class Conexion
     {
+        #region singleton
+        private static readonly Conexion _instancia = new Conexion();
+        public static Conexion Instancia
+        {
+            get { return Conexion._instancia; }
+        }
+        #endregion singleton
+
+        #region metodos
+        public SqlConnection conectar()
+        {
+            SqlConnection cn = new SqlConnection();
+            cn.ConnectionString = "whateverthefuckitis :^)";
+            //"Integrated Security=SSPI";
+            return cn;
+        }
+        #endregion metodos
     }
 }
