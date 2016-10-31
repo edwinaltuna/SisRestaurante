6877e9e6fc913e25ff17e1ee5f5d5a4c7632719a
Kenneth Lujan Rosas <elgenio.03@gmail.com>
2016-10-31 01:07:20 -0500

    Agregada clase Producto, y un placeholder de método.

---
+++ SistemaRestaurante/C3_Dominio/Entidades/Producto.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C3_Dominio.Entidades
+{
+    public class Producto
+    {
+        public int id {get;set;}
+        public double precio { get; set; }
+        public string descripcion { get; set; }
+        public DateTime fecha { get; set; }
+        public string imagen { get; set; }
+        public bool estado { get; set; }
+    }
+}
