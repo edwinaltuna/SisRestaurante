d9e765982a3023fbb231bbfc4fe98672c5000d3c
Kenneth Lujan Rosas <elgenio.03@gmail.com>
2016-10-31 01:01:58 -0500

    se removieron atributos que deberian estar en las clases Cliente y Trabajador por separado

--- SistemaRestaurante/C3_Dominio/Entidades/Persona.cs
+++ SistemaRestaurante/C3_Dominio/Entidades/Persona.cs
@@ -8,14 +8,12 @@ namespace C3_Dominio.Entidades
 {
     public class Persona
     {
-        public int idPersona { get; set; }
-        public String nombres { get; set; }
-        public String apellidoPaterno { get; set; }
-        public String apellidoMaterno { get; set; }
-        public String dni { get; set; }
+        public int id { get; set; }
+        public string nombres { get; set; }
+        public string apellidoPaterno { get; set; }
+        public string apellidoMaterno { get; set; }
+        public string dni { get; set; }
         public DateTime fechaNacimiento { get; set; }
-        public String direccion { get; set; }
-        public String telefono { get; set; }
-        public Boolean estado { get; set; }
+        public bool estado { get; set; }
     }
 }
