b11ee2c30a50ce7c6eb530541c3c424b59bfac1d
Kenneth Lujan Rosas <elgenio.03@gmail.com>
2016-10-31 01:07:45 -0500

    Agregada clase TipoProducto.

---
+++ SistemaRestaurante/C3_Dominio/Entidades/TipoProducto.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C3_Dominio.Entidades
+{
+    public class TipoProducto
+    {
+        public int id {get; set;}
+        public string descripcion {get; set;}
+        public bool estado { get; set; } 
+    }
+}
