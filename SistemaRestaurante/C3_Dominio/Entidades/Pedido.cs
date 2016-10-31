73918a0b5a4152113c5859b6c9115b5573d92181
Kenneth Lujan Rosas <elgenio.03@gmail.com>
2016-10-31 01:06:53 -0500

    Pedido, funciones de calculo de total, y agregarDetallePedido

---
+++ SistemaRestaurante/C3_Dominio/Entidades/Pedido.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C3_Dominio.Entidades
+{
+    public class Pedido
+    {
+        int idPedido {get; set;}
+        DateTime fecha {get; set;}
+        Double total {get; set;}
+        int estado {get; set;}
+        Cliente idCliente {get; set;}
+        Mesa mesa {get; set;}
+        List<DetallePedido> detallesPedido {get;set;}    
+
+        public double calcularTotal(){
+            double totalCalculado = 0;
+            foreach (var detalle in detallesPedido)
+            {
+                totalCalculado += detalle.calcularSubTotal();
+            }
+            return totalCalculado;
+        }    
+
+        public void agregarDetallePedido(DetallePedido detalleNuevo)
+        {
+            detallesPedido.add(detalleNuevo);
+        }
+    }
+}
