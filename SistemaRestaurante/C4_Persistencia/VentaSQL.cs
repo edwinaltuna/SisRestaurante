96355c28d59ceb20b7d0d6aac40d286f8046c73d
Kenneth Ubaldo Lujan Rosas <elgenio.03@gmail.com>
2016-11-06 16:08:39 -0500

    @hexc0der VentaSQL rehaciendo despues del error

--- SistemaRestaurante/C4_Persistencia/VentaSQL.cs
+++ SistemaRestaurante/C4_Persistencia/VentaSQL.cs
@@ -3,10 +3,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using C3_Dominio.Entidades;
 
 namespace C4_Persistencia
 {
-    class VentaSQL
+    public class VentaSQL
     {
+        private GestorDAOSQL gestorDAOSQL;
+        public VentaSQL(GestorDAOSQL gestorDAOSQL)
+        {
+            this.gestorDAOSQL = gestorDAOSQL;
+        }
+
+        public List<Venta> Listar()
+        {
+            return new List<Venta>();
+        }
     }
 }
