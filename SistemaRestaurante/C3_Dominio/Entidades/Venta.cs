63a47eae82e60a4222c9537659ba9bf779c3c58d
Kenneth Ubaldo Lujan Rosas <elgenio.03@gmail.com>
2016-11-06 17:45:03 -0500

    fixing identation

--- SistemaRestaurante/C3_Dominio/Entidades/Venta.cs
+++ SistemaRestaurante/C3_Dominio/Entidades/Venta.cs
@@ -6,39 +6,40 @@ using System.Threading.Tasks;
 
 namespace C3_Dominio.Entidades
 {
-   public class Venta
+    public class Venta
     {
-       public int id    {get; set;}
-       public TipoVenta tipoVenta {get; set;}
-       public Pedido pedido {get; set;}
-       private int serie = 0;
-       private int numero = 0;
-       public string serieNumero 
-       {
+        public int id { get; set; }
+        public TipoVenta tipoVenta { get; set; }
+        public Pedido pedido { get; set; }
+
+        private int serie = 0;
+        private int numero = 0;
+        public string serieNumero
+        {
             set
             {
                 serieNumero = value;
             }
-           get
-           {
-               calcularSiguienteSerie(serie);
-               calcularSiguienteNumero(numero);
-               return serie.ToString("3D") + " - " + numero.ToString("10D");
-           }
-       }
+            get
+            {
+                calcularSiguienteSerie(serie);
+                calcularSiguienteNumero(numero);
+                return serie.ToString("3D") + " - " + numero.ToString("10D");
+            }
+        }
 
-       #region metodos
-       public void calcularSiguienteSerie(int serie)
-       {
-           this.serie = serie+1;
-           //TODO: algoritmo para calcular serie 
-       }
-
-       public void calcularSiguienteNumero(int numero)
-       {
-           this.numero =  numero + 1;
+        #region metodos
+        public void calcularSiguienteSerie(int serie)
+        {
+            this.serie = serie + 1;
             //TODO: algoritmo para calcular serie 
-       }
-       #endregion metodos
+        }
+
+        public void calcularSiguienteNumero(int numero)
+        {
+            this.numero = numero + 1;
+            //TODO: algoritmo para calcular serie 
+        }
+        #endregion metodos
     }
 }
