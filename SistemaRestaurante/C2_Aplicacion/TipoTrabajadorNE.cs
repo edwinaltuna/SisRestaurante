83b73c2522f918c3aa60fc3bf084be1e2ebb88fe
PieroTejada <pierotejadatalentos@hotmail.com>
2016-11-06 14:22:41 -0500

    TipoTrabajadorNE

---
+++ SistemaRestaurante/C2_Aplicacion/TipoTrabajadorNE.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using C2_Aplicacion;
+using C3_Dominio.Entidades;
+using C4_Persistencia;
+
+namespace C2_Aplicacion
+{
+    public class tipoTrabajadorNE
+    {
+        #region Singleton
+        private static readonly tipoTrabajadorNE _instancia = new tipoTrabajadorNE();
+        public static tipoTrabajadorNE Instancia
+        {
+            get { return tipoTrabajadorNE._instancia; }
+        }
+        #endregion Singleton
+        #region methods
+
+        public List<TipoTrabajador> ListarTipoTrabajadores()
+        {
+            try
+            {
+                return TipoTrabajadorDA.Instancia.ListarTrabajadores();
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+
+        public Boolean RegistrarTipoTrabajador(TipoTrabajador t)
+        {
+            try
+            {
+                return TipoTrabajadorDA.Instancia.RegistrarTipoTrabajador(t);
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        #endregion
+
+        public TipoTrabajador ListarTipoTrabajadores(short id)
+        {
+            try
+            {
+                return TipoTrabajadorDA.Instancia.ListarTrabajadorPorId(id);
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+    }
+
+
+
+}
+
+
+
+
