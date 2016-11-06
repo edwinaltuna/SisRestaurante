59fee82c2e8d595be0209b8b5a3a17cea2a37ce4
LMErika <erikiita_18@hotmail.com>
2016-11-06 03:39:01 -0500

    Reporte : Entidad- DAO

--- SistemaRestaurante/C4_Persistencia/VentaDAO.cs
+++ SistemaRestaurante/C4_Persistencia/VentaDAO.cs
@@ -22,41 +22,6 @@ namespace C4_Persistencia
         }
 
 
-       /* public List<VentaDAO> ListInstitutionType()
-        {
-            //SqlCommand cmd = null;
-            //SqlDataReader dr = null;
-            //List<VentaDAO> lstobjVenta = null;
-
-            //try
-            //{
-            //    cmd = new SqlCommand("ReporteMensual",gestorDAOSQL.ObtenerComandoSQL());
-            //    cmd.CommandType = CommandType.StoredProcedure;
-            //    dr = cmd.ExecuteReader();
-            //    if (dr.HasRows)
-            //    {
-            //        lstobj = new List<clsInstitutionType>();
-            //        while (dr.Read())
-            //        {
-            //            clsInstitutionType obj = new clsInstitutionType();
-            //            obj.Description = dr.GetColumnValue<String>("DESCRIPTION");
-            //            obj.ID = dr.GetColumnValue<Int32>("ID_INSTITUTION_TYPE");
-            //            lstobj.Add(obj);
-            //        }
-            //    }
-            //}
-            //catch (Exception ex)
-            //{
-            //    lstobj = null;
-            //    Entity.Errors.Add(new BaseEntity.ListError(ex, "No found."));
-            //}
-            //finally
-            //{
-            //    clsConnection.DisposeCommand(cmd);
-            //}
-            //return lstobj;
-        }   
-        */
 
        
     }
