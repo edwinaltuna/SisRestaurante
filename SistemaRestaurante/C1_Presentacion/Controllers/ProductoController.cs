ba81cacbe7727a68f32a59126edb8be1d7682ae0
PieroTejada <pierotejadatalentos@hotmail.com>
2016-11-06 20:33:16 -0500

    ProductoController

---
+++ SistemaRestaurante/C1_Presentacion/Controllers/ProductoController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using C2_Aplicacion;
+using C3_Dominio.Entidades;
+
+namespace C1_Presentacion.Controllers
+{
+    public class ProductoController : Controller
+    {
+        //
+        // GET: /Producto/
+
+        gestionarProductoServices obj = new gestionarProductoServices();
+        Producto data = new Producto();
+        // GET: /Trabajador/
+        public ActionResult Index()
+        {
+            List<Producto> listProducto = null;
+            listProducto = obj.ListarProductos().ToList();
+            return View(listProducto);
+        }
+
+    }
+}
