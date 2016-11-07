9a8636c7e6ec6e6da8eb359c6da8293232fc59e9
PieroTejada <pierotejadatalentos@hotmail.com>
2016-11-06 19:57:00 -0500

    Listar Producto

--- SistemaRestaurante/C4_Persistencia/ProductoSQL.cs
+++ SistemaRestaurante/C4_Persistencia/ProductoSQL.cs
@@ -21,6 +21,45 @@ namespace C4_Persistencia
         #endregion Singleton
 
         #region metodos
+
+        public List<Producto> ListarProductos()
+        {
+            SqlCommand cmd = null;
+            List<Producto> listarProducto = new List<Producto>();
+            try
+            {
+                using (SqlConnection cn = GestorDAOSQL.Instancia.abrirConexion())
+                {
+                    cmd = new SqlCommand("listarProducto", cn);
+                    cmd.CommandType = CommandType.StoredProcedure;
+
+                    cn.Open();
+                    SqlDataReader dr = cmd.ExecuteReader();
+                    while (dr.Read())
+                    {
+                        Producto p = new Producto();
+                        p.id = Convert.ToInt16(dr["idProducto"]);
+                        p.precio = Convert.ToDouble(dr["precio"]);
+
+                        p.descripcion = dr["descripcion"].ToString();
+                        //t.descripcion = dr["dni"].ToString();
+                        p.fecha = Convert.ToDateTime(dr["fecha"]);
+
+                        p.imagen = dr["imagen"].ToString();
+                        p.estado = Convert.ToBoolean(dr["estado"]);
+
+                        listarProducto.Add(p);
+                    }
+                }
+                return listarProducto;
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+
+        }
+
         #endregion
     }
 }
