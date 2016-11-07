ee166b85363ce1411ed8f942415180b6ce0bb54a
PieroTejada <pierotejadatalentos@hotmail.com>
2016-11-06 19:51:06 -0500

    ProductoSQL

---
+++ SistemaRestaurante/C4_Persistencia/ProductoSQL.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using C3_Dominio;
+using C3_Dominio.Entidades;
+using System.Data;
+
+namespace C4_Persistencia
+{
+    public class ProductoSQL
+    {
+        #region Singleton
+        private static readonly ProductoSQL _instancia = new ProductoSQL();
+        public static ProductoSQL Instancia
+        {
+            get { return ProductoSQL._instancia; }
+        }
+        #endregion Singleton
+
+        #region metodos
+        #endregion
+    }
+}
