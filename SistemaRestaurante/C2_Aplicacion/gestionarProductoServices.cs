32e9433afcd0961e820d9d499438762e46ec97d5
PieroTejada <pierotejadatalentos@hotmail.com>
2016-11-06 20:00:19 -0500

    Listar NE

---
+++ SistemaRestaurante/C2_Aplicacion/gestionarProductoServices.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using C3_Dominio.Entidades;
+using C4_Persistencia;
+
+namespace C2_Aplicacion
+{
+    public class gestionarProductoServices
+    {
+        #region Singleton
+        private static readonly gestionarProductoServices _instancia = new gestionarProductoServices();
+        public static gestionarProductoServices Instancia
+        {
+            get { return gestionarProductoServices._instancia; }
+        }
+        #endregion Singleton
+        #region methods
+
+        public List<Producto> ListarTipoProductos()
+        {
+            try
+            {
+                return ProductoSQL.Instancia.ListarProductos();
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+        #endregion
+    }
+}
